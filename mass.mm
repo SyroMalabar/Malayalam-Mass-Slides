e9bbd145c481a8ba5e6dbce7a310b61dec17aed9
Syro Malabar <syromalabarottawa.it@gmail.com>
2017-03-13 02:50:47 -0400

    add coloring scheme

--- mass.mm
+++ mass.mm
@@ -11,43 +11,108 @@
 \title{<m> kuR{}baana </m>}
 \date{}
 
+\newcommand{\Priest}[1]{\color{white}#1}
+\newcommand{\People}[1]{\color{yellow}#1}
+
 \begin{document}
 
 \frame{\titlepage}
  
 \begin{frame}[allowframebreaks]
 \frametitle{<m>kurishaTayaaLaM</m>}
-<m>
-kaaR{}mmi:
-
+\Priest{<m>
 pitaavinReyum putranReyum parishuddhaatmaavinReyum naamattil.
+</m>}
 
-samu:
-
+\People{<m>
 aammeen.
-</m>
+</m>}
 \end{frame}
-
 
 \begin{frame}[allowframebreaks]
 \frametitle{<m>mishihaayuTe kala+pana</m>}
-<m>
+\Priest{<m>
 annaappesahaattirunaaLil\\
 kaR{}ttaavaruLiya kala+panapool\\
 tirunaamattil ceeR{}nniiTaam\\
-orumayoTii baliyaR{}ppikkaam.\\
+orumayoTii baliyaR{}ppikkaam.
+</m>}
+
+\People{<m>
 anuran~a+jitaraaya+ttiiR{}nniiTaam\\
 navamoru piiThamorukkiiTaam\\
 guruvin sneehamoTiiyaagam\\
-tirumunpaakeyaNacciiTaam.\\
-</m>
+tirumunpaakeyaNacciiTaam.
+</m>}
+\end{frame}
+
+\begin{frame}[allowframebreaks]
+\frametitle{<m>maalaakhamaaruTe kiiR{}ttanaM</m>}
+\Priest{<m>
+atyunnatan"n"aLil daivattinu stuti.
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
+
+\Priest{<m>
+bhuumiyil manuSyaR{}kku samaadhaanavum pratyaashayum eppoozhum enneekkum.
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
 \end{frame}
 
 \begin{frame}[allowframebreaks]
 \frametitle{<m></m>}
-<m>
-kaaR{}mmi:
-samu:
-</m>
+\Priest{<m>
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
 \end{frame}
+
+\begin{frame}[allowframebreaks]
+\frametitle{<m></m>}
+\Priest{<m>
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
+\end{frame}
+
+\begin{frame}[allowframebreaks]
+\frametitle{<m></m>}
+\Priest{<m>
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
+\end{frame}
+
+\begin{frame}[allowframebreaks]
+\frametitle{<m></m>}
+\Priest{<m>
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
+\end{frame}
+
+\begin{frame}[allowframebreaks]
+\frametitle{<m></m>}
+\Priest{<m>
+</m>}
+
+\People{<m>
+aammeen.
+</m>}
+\end{frame}
+
 \end{document}
